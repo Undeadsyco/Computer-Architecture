03caf17a144179d89fb8ad3fc223d74bb5f46aeb
Brandon Santana <BrandonS1123581321@gmail.com>
2022-11-26 10:43:36 -0600

    created first build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, '/'),
+    path: path.resolve(__dirname, './'),
   },
   mode: 'development',
   module: {
